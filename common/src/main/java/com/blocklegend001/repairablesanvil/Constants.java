1c115fd7627cce42005c00afcb520430cdfdf9da
BlockLegend001 <rosarioarcamone@gmail.com>
2026-06-21 16:04:17 +0200

    Added 26.1

---
+++ common/src/main/java/com/blocklegend001/repairablesanvil/Constants.java
@@ -0,0 +1,11 @@
+package com.blocklegend001.repairablesanvil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "repairablesanvil";
+    public static final String MOD_NAME = "RepairablesAnvil";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
